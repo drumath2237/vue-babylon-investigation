779480139f38ba13616b6fa3434118f9d0949435
drumath2237 <2237k.drumath@gmail.com>
2023-07-17 15:31:28 +0900

    :sparkles: babylon用にコード整理

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas id="renderCanvas"></canvas>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+#renderCanvas {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  border: none !important;
+  outline: none !important;
+  display: block;
 }
 </style>
