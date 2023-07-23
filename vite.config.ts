8e823487cdc94c5ea88bf3fd3eb41c302c8eac21
drumath2237 <2237k.drumath@gmail.com>
2023-07-23 22:42:34 +0900

    :green_heart: ci用のビルド設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  // for github pages
+  if (mode === "production") {
+    return {
+      plugins: [vue()],
+      base: "/vue-babylon-investigation/",
+    };
+  }
+
+  return {
+    plugins: [vue()],
+  };
+});
