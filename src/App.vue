d3ec22a0c1c03155fc946d7550bf08f04e6f3325
drumath2237 <2237k.drumath@gmail.com>
2023-07-30 16:05:06 +0900

    :sparkles: ちょとApp.vueを変更

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
-import { Vector3 } from "@babylonjs/core";
 import BabylonScene from "./components/babylonScene.vue";
 import Box from "./components/box";
 import StandardMaterial from "./components/standardMaterial";
 import ArcRotateCamera from "./components/arcRotateCamera";
+import { reactive } from "vue";
+
+const position = reactive({
+  x: 0,
+  y: 0.05,
+  z: 0,
+});
 </script>
 
 <template>
   <BabylonScene :antialias="true">
     <ArcRotateCamera
-      :name="`camera`"
-      :alpha="0"
-      :beta="Math.PI / 2"
-      :radius="1.5"
-      :target="Vector3.Zero()"
+      :name="'camera'"
+      :alpha="Math.PI / 3"
+      :beta="Math.PI / 3"
+      :radius="0.5"
+      :target="position"
     ></ArcRotateCamera>
-    <Box :position="{ x: 0, y: 0.05, z: 0 }" :size="0.1">
-      <StandardMaterial :color="{ r: 1, g: 0, b: 1 }"></StandardMaterial>
+    <Box :position="position" :size="0.1">
+      <StandardMaterial :color="{ r: 0, g: 1, b: 1 }"></StandardMaterial>
     </Box>
   </BabylonScene>
 </template>
